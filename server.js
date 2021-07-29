d7d46c8fc335cb066b2cdf26b0234127507332da
bob.w.perez <bob.w.perez@gmail.com>
2021-07-29 15:10:48 -0400

    ok

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const server = express().use((req,res) => res.sendFile('/index.html', { root: __dirname})).listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express()
+  .use((req, res) => res.sendFile("/index.html", { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 // httpServer.listen(3000);
 let users = {};
+let userName;
 
-io.on('connection', socket => {
-  socket.on('new-user', userName => {
+io.on("connection", (socket) => {
+  socket.on("new-user", (userName) => {
     users[socket.id] = userName;
-    socket.broadcast.emit('user-connected', userName)
+    socket.broadcast.emit("user-connected", userName);
   });
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, userName: users[socket.id] });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      userName: users[socket.id],
+    });
   });
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
     users[socket.id] = userName;
     delete users[socket.id];
-  })
+  });
 });
